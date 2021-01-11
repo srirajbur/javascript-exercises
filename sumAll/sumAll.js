2d4839ecba1a05c3381904ac81516f18454a3f28
Sriraj Bura <sriraj.bura16@gmail.com>
2021-01-10 21:36:13 -0500

    finished sumall

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
+const sumAll = function(first, sec) {
+  if(((first || sec) < 0) || (typeof(first && sec) !== "number")){
+    return "ERROR"
+  }
+  let large;
+  let small;
+  if(first > sec){
+    large = first
+    small = sec
+  }
+  if(sec > first){
+    large = sec
+    small = first
+  }
+  let sum = 0
 
+  for(let i = small; i <= large; i++){
+    sum += i
+  }
+  return sum
 }
+
 
 module.exports = sumAll
