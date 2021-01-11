b8b2554325e0bcd7823e421b4b392c88a30b7135
Sriraj Bura <sriraj.bura16@gmail.com>
2021-01-10 21:04:23 -0500

    finished repeatString

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+  if(num < 0) return "ERROR"
+  let final = ""
+  for(let i = 1; i <= num; i++){
+    final += string
+  }
+  return final
 }
 
 module.exports = repeatString
